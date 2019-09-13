19db2f0058cffd4e957735b24d4919ec4dd8be9a
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-13 20:40:56 +0700

    Add files via upload

---
+++ source.sh
@@ -0,0 +1,53 @@
+clear
+figlet Welcome
+sleep 0.1
+figlet To
+sleep 0.1
+clear
+figlet My
+sleep 0.1
+figlet Termux!
+sleep 0.5
+clear
+sleep 1
+echo "______ _                    _"
+echo "(_) |  | |                  | |"
+echo "    |  | |     __,   _  _   | |   ,"
+echo " _ |  |/ \   /  |  / |/ |  |/_) / \_"
+echo " (_/   |   |_/\_/|_/  |  |_/| \_/ \/"
+sleep 0.9
+clear
+echo "      _____                   _______         "
+echo "     /\    \                 /::\    \        "
+echo "    /::\    \               /::::\    \       "
+echo "    \::\    \             /::::::\    \      "
+echo "     \:::\    \           /::::::::\    \     "
+echo "      \:::\    \         /:::/~~\:::\    \    "
+echo "      \:::\    \       /:::/    \:::\    \   "
+echo "       /::::\    \     /:::/    / \:::\    \  "
+echo "      /::::::\    \   /:::/____/   \:::\____\ "
+echo "     /:::/\:::\    \ |:::|    |     |:::|    |"
+echo "    /:::/  \:::\____\|:::|____|     |:::|    |"
+echo "   /:::/    \::/    / \:::\    \   /:::/    / "
+echo "  /:::/    / \/____/   \:::\    \ /:::/    /  "
+echo " /:::/    /             \:::\    /:::/    /   "
+echo "/:::/    /               \:::\__/:::/    /    "
+echo "\::/    /                 \::::::::/    /     "
+echo " \/____/                   \::::::/    /      "
+echo "                            \::::/    /       "
+echo "                             \::/____/        "
+echo "                              ~~              "
+echo "                                              "
+echo "                                              "
+sleep 0.9
+clear
+echo "Ａｌｌａｈ"
+sleep 0.9
+clear
+
+command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+PS1='root@MrY0uKn0wM3~# '
+ 
